b8525dbb657a91581060fe2ea85a27f24c69baf4
Yann IRBAH <yann.irbah@protonmail.com>
2020-09-04 15:14:56 +0200

    Initial commit with sparta-cli

---
+++ Formula/sparta-cli.rb
@@ -0,0 +1,19 @@
+class SpartaCli < Formula
+  desc "The Fewlines Sparta CLI"
+  homepage "https://sparta.fewlines.dev"
+  url "https://fewlines-brew-packages.s3.eu-west-3.amazonaws.com/sparta-cli/sparta-v1.0.0-darwin-x64.tar.gz"
+  sha256 "0f5870954b2c7d475edf1d84c6e4e027e36bf83027edf65f37d05a81032346f5"
+  license "MIT"
+  depends_on "heroku/brew/heroku-node"
+
+  def install
+    inreplace "bin/sparta", /^CLIENT_HOME=/, "export HEROKU_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/sparta", "\"$DIR/node\"", "#{Formula["heroku-node"].opt_share}/node"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/sparta"
+  end
+
+  test do
+    system bin/"heroku", "version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Fewlines-education Sparta
+
+## How do I install these formulae?
+`brew install fewlines-education/sparta/<formula>`
+
+Or `brew tap fewlines-education/sparta` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
