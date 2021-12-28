291608308ce95343fed8b483eed42c220686003f
Vincent Billey <fenn@fewlines.co>
2021-12-28 11:33:11 +0100

    v1.2.0

--- Formula/sparta-cli.rb
+++ Formula/sparta-cli.rb
@@ -1,8 +1,8 @@
 class SpartaCli < Formula
   desc "Fewlines Sparta CLI"
   homepage "https://github.com/fewlinesco/sparta-cli"
-  url "https://github.com/fewlinesco/sparta-cli/releases/download/1.1.0/sparta-cli-v1.1.0.tar.gz"
-  sha256 "a0a9d1d346255ed3c1022dbdc9dc641e754aabb8a1a07ecaa98b461ffe2c0f44"
+  url "https://github.com/fewlinesco/sparta-cli/releases/download/1.2.0/sparta-cli-v1.2.0.tar.gz"
+  sha256 "64afe72218f996d65a0d872e7b398e71e0b2d0c0e70b9a5d89a5bc57a1782bdf"
   license "MIT"
   depends_on "heroku/brew/heroku-node"
 
